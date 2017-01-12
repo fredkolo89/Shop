6dd45d5d0f90b8790f9b68a364f597fa38d6205f
Mazur <michalmazur3@gmail.com>
2017-01-12 22:31:20 +0100

    85

--- projekt Xamarin/Texture/PaintingView.cs
+++ projekt Xamarin/Texture/PaintingView.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 using OpenTK.Graphics;
@@ -13,297 +14,316 @@ using Android.Graphics;
 using Android.Util;
 using Android.Views;
 
-namespace Mono.Samples.TexturedCube {
+namespace Mono.Samples.TexturedCube
+{
 
-	class PaintingView : AndroidGameView
-	{
-	     float rotateX;
-		float prevx, prevy;
-		float xangle, yangle;
-		int [] textureIds;
-		int cur_texture;
-		int width, height;
-		Context context;
+    class PaintingView : AndroidGameView
+    {
+        float rotateX;
+        float prevx, prevy;
+        float xangle, yangle;
+        int[] textureIds;
+        int cur_texture;
+        int width, height;
+        Context context;
 
-		public PaintingView (Context context, IAttributeSet attrs) :
-			base (context, attrs)
-		{
-			Initialize ();
-		}
+        public PaintingView(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
 
-		public PaintingView (IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
-			: base (handle, transfer)
-		{
-			Initialize ();
-		}
+        public PaintingView(IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
+            : base(handle, transfer)
+        {
+            Initialize();
+        }
 
-		private void Initialize ()
-		{
-			textureIds = new int[7];
-			context = Context;
-			xangle = 45;
-			yangle = 45;
+        private void Initialize()
+        {
+            textureIds = new int[7];
+            context = Context;
+            xangle = 45;
+            yangle = 45;
 
-			Resize += delegate {
-				height = Height;
-				width = Width;
-				SetupCamera ();
-				RenderCube ();
-			};
-		}
+            Resize += delegate
+            {
+                height = Height;
+                width = Width;
+                SetupCamera();
+                RenderCube();
+            };
+        }
 
-		// This method is called everytime the context needs
-		// to be recreated. Use it to set any egl-specific settings
-		// prior to context creation
-		protected override void CreateFrameBuffer ()
-		{
-			ContextRenderingApi = GLVersion.ES1;
+        // This method is called everytime the context needs
+        // to be recreated. Use it to set any egl-specific settings
+        // prior to context creation
+        protected override void CreateFrameBuffer()
+        {
+            ContextRenderingApi = GLVersion.ES1;
 
-			// the default GraphicsMode that is set consists of (16, 16, 0, 0, 2, false)
-			try {
-				Log.Verbose ("TexturedCube", "Loading with default settings");
+            // the default GraphicsMode that is set consists of (16, 16, 0, 0, 2, false)
+            try
+            {
+                Log.Verbose("TexturedCube", "Loading with default settings");
 
-				// if you don't call this, the context won't be created
-				base.CreateFrameBuffer ();
-				return;
-			} catch (Exception ex) {
-				Log.Verbose ("TexturedCube", "{0}", ex);
-			}
+                // if you don't call this, the context won't be created
+                base.CreateFrameBuffer();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Verbose("TexturedCube", "{0}", ex);
+            }
 
-			// Fallback modes
-			// If the first attempt at initializing the surface with a default graphics
-			// mode fails, then the app can try different configurations. Devices will
-			// support different modes, and what is valid for one might not be valid for
-			// another. If all options fail, you can set all values to 0, which will
-			// ask for the first available configuration the device has without any
-			// filtering.
-			// After a successful call to base.CreateFrameBuffer(), the GraphicsMode
-			// object will have its values filled with the actual values that the
-			// device returned.
+            // Fallback modes
+            // If the first attempt at initializing the surface with a default graphics
+            // mode fails, then the app can try different configurations. Devices will
+            // support different modes, and what is valid for one might not be valid for
+            // another. If all options fail, you can set all values to 0, which will
+            // ask for the first available configuration the device has without any
+            // filtering.
+            // After a successful call to base.CreateFrameBuffer(), the GraphicsMode
+            // object will have its values filled with the actual values that the
+            // device returned.
 
 
-			// This is a setting that asks for any available 16-bit color mode with no
-			// other filters. It passes 0 to the buffers parameter, which is an invalid
-			// setting in the default OpenTK implementation but is valid in some
-			// Android implementations, so the AndroidGraphicsMode object allows it.
-			try {
-				Log.Verbose ("TexturedCube", "Loading with custom Android settings (low mode)");
-				GraphicsMode = new AndroidGraphicsMode (16, 0, 0, 0, 0, false);
+            // This is a setting that asks for any available 16-bit color mode with no
+            // other filters. It passes 0 to the buffers parameter, which is an invalid
+            // setting in the default OpenTK implementation but is valid in some
+            // Android implementations, so the AndroidGraphicsMode object allows it.
+            try
+            {
+                Log.Verbose("TexturedCube", "Loading with custom Android settings (low mode)");
+                GraphicsMode = new AndroidGraphicsMode(16, 0, 0, 0, 0, false);
 
-				// if you don't call this, the context won't be created
-				base.CreateFrameBuffer ();
-				return;
-			} catch (Exception ex) {
-				Log.Verbose ("TexturedCube", "{0}", ex);
-			}
+                // if you don't call this, the context won't be created
+                base.CreateFrameBuffer();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Verbose("TexturedCube", "{0}", ex);
+            }
 
-			// this is a setting that doesn't specify any color values. Certain devices
-			// return invalid graphics modes when any color level is requested, and in
-			// those cases, the only way to get a valid mode is to not specify anything,
-			// even requesting a default value of 0 would return an invalid mode.
-			try {
-				Log.Verbose ("TexturedCube", "Loading with no Android settings");
-				GraphicsMode = new AndroidGraphicsMode (0, 4, 0, 0, 0, false);
+            // this is a setting that doesn't specify any color values. Certain devices
+            // return invalid graphics modes when any color level is requested, and in
+            // those cases, the only way to get a valid mode is to not specify anything,
+            // even requesting a default value of 0 would return an invalid mode.
+            try
+            {
+                Log.Verbose("TexturedCube", "Loading with no Android settings");
+                GraphicsMode = new AndroidGraphicsMode(0, 4, 0, 0, 0, false);
 
-				// if you don't call this, the context won't be created
-				base.CreateFrameBuffer ();
-				return;
-			} catch (Exception ex) {
-				Log.Verbose ("TexturedCube", "{0}", ex);
-			}
-			throw new Exception ("Can't load egl, aborting");
-		}
+                // if you don't call this, the context won't be created
+                base.CreateFrameBuffer();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Verbose("TexturedCube", "{0}", ex);
+            }
+            throw new Exception("Can't load egl, aborting");
+        }
 
-		protected override void OnLoad (EventArgs e)
-		{
-			GL.ShadeModel (All.Smooth);
-			GL.ClearColor (0, 0, 0, 1);
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ShadeModel(All.Smooth);
+            GL.ClearColor(0, 0, 0, 1);
 
-			GL.ClearDepth (1.0f);
-			GL.Enable (All.DepthTest);
-			GL.DepthFunc (All.Lequal);
+            GL.ClearDepth(1.0f);
+            GL.Enable(All.DepthTest);
+            GL.DepthFunc(All.Lequal);
 
-			GL.Enable (All.CullFace);
-			GL.CullFace (All.Back);
+            GL.Enable(All.CullFace);
+            GL.CullFace(All.Back);
 
-			GL.Hint (All.PerspectiveCorrectionHint, All.Nicest);
+            GL.Hint(All.PerspectiveCorrectionHint, All.Nicest);
 
-			// create texture ids
-			GL.Enable (All.Texture2D);
-			GL.GenTextures (7, textureIds);
+            // create texture ids
+            GL.Enable(All.Texture2D);
+            GL.GenTextures(7, textureIds);
 
-			LoadTexture (context, Resource.Drawable.photo1, textureIds [0]);
-           LoadTexture(context, Resource.Drawable.photo2, textureIds[1]);
+            LoadTexture(context, Resource.Drawable.photo1, textureIds[0]);
+            LoadTexture(context, Resource.Drawable.photo2, textureIds[1]);
             LoadTexture(context, Resource.Drawable.photo3, textureIds[2]);
             LoadTexture(context, Resource.Drawable.photo4, textureIds[3]);
             LoadTexture(context, Resource.Drawable.photo5, textureIds[4]);
             LoadTexture(context, Resource.Drawable.photo6, textureIds[5]);
             LoadTexture(context, Resource.Drawable.pattern, textureIds[6]);
 
- 
 
 
-			SetupCamera ();
-			RenderCube ();
-		}
 
-		void SetupCamera ()
-		{
-			width = Width;
-			height = Height;
+            SetupCamera();
+            RenderCube();
+        }
 
-			GL.Viewport(0, 0, width, height);
-			// setup projection matrix
-			GL.MatrixMode(All.Projection);
-			GL.LoadIdentity();
+        void SetupCamera()
+        {
+            width = Width;
+            height = Height;
 
-			// gluPerspective
-			Matrix4 m = Matrix4.CreatePerspectiveFieldOfView (ToRadians (45.0f), (float)width / (float)height, 1.0f, 100.0f);
-			float [] perspective_m = new float [16];
+            GL.Viewport(0, 0, width, height);
+            // setup projection matrix
+            GL.MatrixMode(All.Projection);
+            GL.LoadIdentity();
 
-			int i = 0;
-			perspective_m [i + 0] = m.Row0.X; perspective_m [i + 1] = m.Row0.Y;
-			perspective_m [i + 2] = m.Row0.Z; perspective_m [i + 3] = m.Row0.W;
-			i += 4;
+            // gluPerspective
+            Matrix4 m = Matrix4.CreatePerspectiveFieldOfView(ToRadians(45.0f), (float)width / (float)height, 1.0f, 100.0f);
+            float[] perspective_m = new float[16];
 
-			perspective_m [i + 0] = m.Row1.X; perspective_m [i + 1] = m.Row1.Y;
-			perspective_m [i + 2] = m.Row1.Z; perspective_m [i + 3] = m.Row1.W;
-			i += 4;
+            int i = 0;
+            perspective_m[i + 0] = m.Row0.X; perspective_m[i + 1] = m.Row0.Y;
+            perspective_m[i + 2] = m.Row0.Z; perspective_m[i + 3] = m.Row0.W;
+            i += 4;
 
-			perspective_m [i + 0] = m.Row2.X; perspective_m [i + 1] = m.Row2.Y;
-			perspective_m [i + 2] = m.Row2.Z; perspective_m [i + 3] = m.Row2.W;
-			i += 4;
+            perspective_m[i + 0] = m.Row1.X; perspective_m[i + 1] = m.Row1.Y;
+            perspective_m[i + 2] = m.Row1.Z; perspective_m[i + 3] = m.Row1.W;
+            i += 4;
 
-			perspective_m [i + 0] = m.Row3.X; perspective_m [i + 1] = m.Row3.Y;
-			perspective_m [i + 2] = m.Row3.Z; perspective_m [i + 3] = m.Row3.W;
+            perspective_m[i + 0] = m.Row2.X; perspective_m[i + 1] = m.Row2.Y;
+            perspective_m[i + 2] = m.Row2.Z; perspective_m[i + 3] = m.Row2.W;
+            i += 4;
 
-			GL.LoadMatrix (perspective_m);
-		}
+            perspective_m[i + 0] = m.Row3.X; perspective_m[i + 1] = m.Row3.Y;
+            perspective_m[i + 2] = m.Row3.Z; perspective_m[i + 3] = m.Row3.W;
 
-		public override bool OnTouchEvent (MotionEvent e)
-		{
-			base.OnTouchEvent (e);
-			if (e.Action == MotionEventActions.Down) {
-			    prevx = e.GetX ();
-			    prevy = e.GetY ();
-			}
-			if (e.Action == MotionEventActions.Move) {
-			    float e_x = e.GetX ();
-			    float e_y = e.GetY ();
+            GL.LoadMatrix(perspective_m);
+        }
 
-			    float xdiff = (prevx - e_x);
-			    float ydiff = (prevy - e_y);
-			    xangle = xangle + ydiff;
-			    yangle = yangle + xdiff;
-			    prevx = e_x;
-			    prevy = e_y;
-			}
-			if (e.Action == MotionEventActions.Down || e.Action == MotionEventActions.Move)
-				RenderCube ();
-			return true;
-		}
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            base.OnTouchEvent(e);
+            if (e.Action == MotionEventActions.Down)
+            {
+                prevx = e.GetX();
+                prevy = e.GetY();
+            }
+            if (e.Action == MotionEventActions.Move)
+            {
+                float e_x = e.GetX();
+                float e_y = e.GetY();
 
-		protected override void OnUnload (EventArgs e)
-		{
-			GL.DeleteTextures (6, textureIds);
-		}
+                float xdiff = (prevx - e_x);
+                float ydiff = (prevy - e_y);
+                xangle = xangle + ydiff;
+               // yangle = yangle + xdiff;
+                prevx = e_x;
+                prevy = e_y;
+            }
+            if (e.Action == MotionEventActions.Down || e.Action == MotionEventActions.Move)
+                RenderCube();
+            return true;
+        }
 
-		public void SwitchTexture ()
-		{   Random rnd = new Random(DateTime.Now.Millisecond);
-			//cur_texture = (cur_texture + 1) % textureIds.Length;
-        cur_texture = rnd.Next(0, textureIds.Length);
-          // RenderCube ();
-		}
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.DeleteTextures(6, textureIds);
+        }
 
-		void RenderCube ()
-		{
-			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			GL.MatrixMode(All.Modelview);
-			GL.LoadIdentity();
+        public void SwitchTexture()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
 
-			// draw cube
+            textureIds = textureIds.Select(item => new { item, order = rnd.Next() })
+            .OrderBy(x => x.order)
+            .Select(x => x.item)
+            .ToArray();
+            RenderCube();
+        }
 
-			GL.Translate(0, 0, -6);
-			GL.Rotate(-xangle, 1, 0, 0);
-			GL.Rotate(-yangle, 0, 1, 0);
+        void RenderCube()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(All.Modelview);
+            GL.LoadIdentity();
+
+            // draw cube
+
+            GL.Translate(0, 0, -6);
+            GL.Rotate(-xangle, 1, 0, 0);
+            GL.Rotate(-yangle, 0, 1, 0);
 
 
             GL.EnableClientState(All.VertexArray);
             GL.EnableClientState(All.TextureCoordArray);
             rotateX += xangle;
             Console.WriteLine(rotateX);
-		    
-		
-			for (int i = 0; i < 6; i++) // draw each face
-			{
 
-                if (rotateX>160 && rotateX<180)
-                {
-                    cur_texture =  textureIds[6];
-                    rotateX = 0;
-                }
-                
+
+            for (int i = 0; i < 6; i++) // draw each face
+            {
+
+                //if (rotateX>160 && rotateX<180)
+                //{
+                //    cur_texture =  textureIds[6];
+                //    rotateX = 0;
+                //}
+
                 GL.BindTexture(All.Texture2D, textureIds[i]);
-				float [] v = cubeVertexCoords [i];
-				float [] t = cubeTextureCoords [i];
-				// pin the data, so that GC doesn't move them, while used
-				// by native code
-				unsafe {
-					fixed (float* pv = v, pt = t) {
-						GL.VertexPointer(3, All.Float, 0, new IntPtr (pv));
-						GL.TexCoordPointer(2, All.Float, 0, new IntPtr (pt));
-						GL.DrawArrays(All.TriangleFan, 0, 4);
-						GL.Finish ();
-					}
-				}
-			}
+                float[] v = cubeVertexCoords[i];
+                float[] t = cubeTextureCoords[i];
+                // pin the data, so that GC doesn't move them, while used
+                // by native code
+                unsafe
+                {
+                    fixed (float* pv = v, pt = t)
+                    {
+                        GL.VertexPointer(3, All.Float, 0, new IntPtr(pv));
+                        GL.TexCoordPointer(2, All.Float, 0, new IntPtr(pt));
+                        GL.DrawArrays(All.TriangleFan, 0, 4);
+                        GL.Finish();
+                    }
+                }
+            }
 
 
 
 
 
-			GL.DisableClientState(All.VertexArray);
-			GL.DisableClientState(All.TextureCoordArray);
+            GL.DisableClientState(All.VertexArray);
+            GL.DisableClientState(All.TextureCoordArray);
 
-			SwapBuffers ();
-		}
+            SwapBuffers();
+        }
 
-		protected override void Dispose (bool disposing)
-		{
-			base.Dispose (disposing);
-			GL.DeleteTextures (6, textureIds);
-		}
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            GL.DeleteTextures(6, textureIds);
+        }
 
-		protected override void OnResize (EventArgs e)
-		{
-			base.OnResize (e);
-			RenderCube ();
-		}
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            RenderCube();
+        }
 
-		public static float ToRadians (float degrees)
-		{
-			//pi/180
-			//FIXME: precalc pi/180
-			return (float) (degrees * (System.Math.PI/180.0));
-		}
+        public static float ToRadians(float degrees)
+        {
+            //pi/180
+            //FIXME: precalc pi/180
+            return (float)(degrees * (System.Math.PI / 180.0));
+        }
 
-		void LoadTexture (Context context, int resourceId, int tex_id)
-		{
-			GL.BindTexture (All.Texture2D, tex_id);
+        void LoadTexture(Context context, int resourceId, int tex_id)
+        {
+            GL.BindTexture(All.Texture2D, tex_id);
 
-			// setup texture parameters
-			GL.TexParameterx (All.Texture2D, All.TextureMagFilter, (int)All.Linear);
-			GL.TexParameterx (All.Texture2D, All.TextureMinFilter, (int)All.Linear);
-			GL.TexParameterx (All.Texture2D, All.TextureWrapS, (int)All.ClampToEdge);
-			GL.TexParameterx (All.Texture2D, All.TextureWrapT, (int)All.ClampToEdge);
+            // setup texture parameters
+            GL.TexParameterx(All.Texture2D, All.TextureMagFilter, (int)All.Linear);
+            GL.TexParameterx(All.Texture2D, All.TextureMinFilter, (int)All.Linear);
+            GL.TexParameterx(All.Texture2D, All.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameterx(All.Texture2D, All.TextureWrapT, (int)All.ClampToEdge);
 
-			Bitmap b = BitmapFactory.DecodeResource (context.Resources, resourceId);
-           
-			Android.Opengl.GLUtils.TexImage2D ((int)All.Texture2D, 0, b, 0); 
-		}
+            Bitmap b = BitmapFactory.DecodeResource(context.Resources, resourceId);
+
+            Android.Opengl.GLUtils.TexImage2D((int)All.Texture2D, 0, b, 0);
+        }
 
 
-		static float[][] cubeVertexCoords = new float[][] {
+        static float[][] cubeVertexCoords = new float[][] {
 			new float[] { // top
 				 1, 1,-1,
 				-1, 1,-1,
@@ -342,7 +362,7 @@ namespace Mono.Samples.TexturedCube {
 			},
 		};
 
-		static float[][] cubeTextureCoords = new float[][] {
+        static float[][] cubeTextureCoords = new float[][] {
 			new float[] { // top
 				1, 0,
 				1, 1,
@@ -382,5 +402,5 @@ namespace Mono.Samples.TexturedCube {
 		};
 
 
-	}
+    }
 }
