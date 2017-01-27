2fa405e1c749eae64e758bb3568af642393af175
Mazur <michalmazur3@gmail.com>
2017-01-27 19:33:32 +0100

    h

---
+++ 45/45/Db/Models/Data3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45.Db.Models
+{ 
+    public class Data3
+    {
+        [Key]
+        public int id { get; set; }
+        public string bezrobocie { get; set; }
+        public string wielkosc_rodziny { get; set; }
+        public string wielkosc_ubostwa { get; set; }
+        public string stan_zdrowotny { get; set; }
+        public bool rezultat { get; set; }
+    }
+}
