a7218c01c270c691b09d9cb340b2ea531f7f3f90
ŁukaszCichoń <lukasz.cichon11@gmail.com>
2017-01-31 17:03:35 +0100

    analiz vol2

---
+++ 45 v2.0/45/Attributecs.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using _45.Db;
+using _45.Db.Models;
+using _45.Tree;
+
+namespace _45
+{
+    public class Attribute
+    {
+        ArrayList mValues;
+        string mName;
+        public object mLabel;
+
+        public Attribute(string name, string[] values)
+        {
+            mName = name;
+            mValues = new ArrayList(values);
+            mValues.Sort();
+        }
+
+        public Attribute(object Label)
+        {
+            mLabel = Label;
+            mName = string.Empty;
+            mValues = null;
+        }
+
+        public string AttributeName
+        {
+            get
+            {
+                return mName;
+            }
+        }
+
+        public string[] values
+        {
+            get
+            {
+                if (mValues != null)
+                    return (string[])mValues.ToArray(typeof(string));
+                else
+                    return null;
+            }
+        }
+
+
+        public bool isValidValue(string value)
+        {
+            return indexValue(value) >= 0;
+        }
+
+        public int indexValue(string value)
+        {
+            if (mValues != null)
+                return mValues.BinarySearch(value);
+            else
+                return -1;
+        }
+
+
+        public override string ToString()
+        {
+            if (mName != string.Empty)
+            {
+                return mName;
+            }
+            else
+            {
+                return mLabel.ToString();
+            }
+        }
+
+        public static Attribute GetAttribute(int pozycja, DataTable tb, List<string> nameColumns)
+        {
+            var tab = tb.AsEnumerable().Select(x => x[pozycja].ToString()).Distinct().ToArray();
+            Attribute attr = new Attribute(nameColumns[pozycja].ToString(), tab);
+            return attr;
+        }
+
+    }
+}
---
+++ 45 v2.0/45/Db/Models/Data1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45.Db.Models
+{
+    public class Data1 
+    {
+        [Key]
+        public int id { get; set; }
+        public string pogoda { get; set; }
+        public string temperatura { get; set; }
+        public string zachmurzenie { get; set; }
+        public string wiatr { get; set; }
+        public bool rezultat { get; set; }
+    }
+}
